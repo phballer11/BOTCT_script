41f331077d4df485ce6b675e84a312f50aa09c08
Peter <yubinhuang11@gmail.com>
2022-09-13 22:40:38 +1200

    added script

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,150 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { BARON, DEMON, DRUNK, MINIONS, OUTSIDERS, role, TOWN_FOLKS } from './images';
+
+interface player {
+  name: string;
+  imgSrc: string;
+  imgAlt: string;
+}
 
 function App() {
+
+  const [data, setData] = useState({
+    names: "",
+    townsfolkCount: 0,
+    outsiderCount: 0,
+    minionCount: 0,
+  })
+
+  const [players, setPlayers] = useState<player[]>([]);
+  const [drunkRole, setDrunkRole] = useState<role>(DRUNK);
+  const [demonRoles, setDemonRoles] = useState<role[]>([]);
+
+  const handleChange = (event: any) => {
+    setData(prev => ({ ...prev, [event.target.name]: event.target.value }));
+
+  };
+
+  const reset = () => { 
+    setPlayers([]);
+    setDemonRoles([]);
+    setDrunkRole(DRUNK);
+   }
+
+  const calculate = () => {
+    reset();
+
+
+    const namesArray = data.names.split(' ');
+    const filteredNames = namesArray.filter((name) => name !== '');
+    const minions = randomGetCharacters(data.minionCount, [...MINIONS]);
+
+    let townsfolkCountsUpdated = data.townsfolkCount;
+    let outsiderCountsUpdated = data.outsiderCount;
+    if (minions.findIndex(m => m.alt === BARON.alt) !== -1) {
+      townsfolkCountsUpdated = +townsfolkCountsUpdated - 2;
+      outsiderCountsUpdated = +outsiderCountsUpdated + 2;
+    }
+
+    const townsfolk = randomGetCharacters(townsfolkCountsUpdated, [...TOWN_FOLKS]);
+    const outsiders = randomGetCharacters(outsiderCountsUpdated, [...OUTSIDERS]);
+
+
+    const unusuedTownsFolk = TOWN_FOLKS.filter((townFolk) => townsfolk.indexOf(townFolk) === -1);
+    const unusuedOutsiders = OUTSIDERS.filter((outsider) => outsiders.indexOf(outsider) === -1);
+
+    const allRoles = [townsfolk, outsiders, minions, DEMON].flat();
+
+    if (filteredNames.length !== allRoles.length) {
+      alert('Please check the number of players and roles');
+      return;
+    }
+
+    let result: player[] = [];
+    filteredNames.forEach(name => {
+      // random pick a role
+      const randomIndex = Math.floor(Math.random() * allRoles.length);
+      const randomRole = allRoles[randomIndex];
+      if (randomRole.alt === DRUNK.alt) {
+        addDrunk(unusuedTownsFolk);
+      }
+
+      allRoles.splice(randomIndex, 1);
+      result.push({ name, imgSrc: randomRole.src, imgAlt: randomRole.alt });
+    });
+    addDemonRoles([unusuedOutsiders, unusuedTownsFolk].flat());
+    setPlayers(result);
+  }
+
+  const addDrunk = (unusuedTownsFolk: role[]) => {
+    const randomIndex = Math.floor(Math.random() * unusuedTownsFolk.length);
+    const randomRole = unusuedTownsFolk[randomIndex];
+    unusuedTownsFolk.splice(randomIndex, 1);
+    setDrunkRole(randomRole);
+  }
+
+  const addDemonRoles = (unusedRoles: role[]) => {
+    for (var i = 0; i < 3; i++) {
+      const randomIndex = Math.floor(Math.random() * unusedRoles.length);
+      const randomRole = unusedRoles[randomIndex];
+      unusedRoles.splice(randomIndex, 1);
+      setDemonRoles(prev => [...prev, randomRole]);
+    }
+
+  }
+
+  const randomGetCharacters = (numberOfRoles: number, roles: role[]) => {
+    const results = [];
+    for (let i = 0; i < numberOfRoles; i++) {
+      const randomIndex = Math.floor(Math.random() * roles.length);
+      results.push(roles[randomIndex]);
+      roles.splice(randomIndex, 1);
+    }
+    return results;
+  }
+
   return (
     <div className="App">
 
-    <label>Enter names, split by ,</label>
-    <input type="text" id="names" />
-    <div>hi</div>
+      <label>Enter names of the players, split by " " space</label>
+      <br />
+      <input
+        style={{ width: '80%' }}
+        type="text"
+        id="names"
+        name="names"
+        onChange={handleChange}
+      // value={names}
+      />
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        flexWrap: 'nowrap',
+        alignContent: 'flex-start',
+        alignItems: 'center'
+      }}>
+        <br />
+        <label>Number of townsfolks:</label>
+        <input type="number" name="townsfolkCount" onChange={handleChange} />
+        <br />
+        <label>Number of outsiders:</label>
+        <input type="number" name="outsiderCount" onChange={handleChange} />
+        <br />
+        <label>Number of minions:</label>
+        <input type="number" name="minionCount" onChange={handleChange} />
+        <br />
+        <br />
+      </div>
+      <button style={{ width: '96px', height: '24px' }} onClick={calculate}>Generate</button>
+      <h2>Number of players: {players.length}</h2>
+      <div className='name-list-container'>
+        {players.map((player, index) => (<div>{index + 1}. {player.name}
+          <img src={player.imgSrc} alt={player.imgAlt} width='112' />
+          {player.imgAlt === DRUNK.alt && <img src={drunkRole.src} alt={drunkRole.alt} width='112' />}
+          {player.imgAlt === DEMON.alt && demonRoles.map((role, index) => <img src={role.src} alt={role.alt} width='112' />)}
+        </div>))}
+      </div>
     </div>
   );
 }
---
+++ src/images.ts
@@ -0,0 +1,85 @@
+// demon
+import imp from './assets/trouble_brewing_images/imp.png'
+
+// minions
+import baron from './assets/trouble_brewing_images/baron.png';
+import scarlert_woman from './assets/trouble_brewing_images/scarlert_woman.png'
+import spy from './assets/trouble_brewing_images/spy.png'
+import poisoner from './assets/trouble_brewing_images/poisoner.png'
+
+// outsiders
+import drunk from './assets/trouble_brewing_images/drunk.png'
+import recluse from './assets/trouble_brewing_images/recluse.png'
+import saint from './assets/trouble_brewing_images/saint.png'
+import butler from './assets/trouble_brewing_images/butler.png'
+
+// townfolks
+import washerwoman from './assets/trouble_brewing_images/washerwoman.png'
+import undertaker from './assets/trouble_brewing_images/undertaker.png'
+import virgin from './assets/trouble_brewing_images/virgin.png'
+import slayer from './assets/trouble_brewing_images/slayer.png'
+import soldier from './assets/trouble_brewing_images/soldier.png'
+import ravenkeeper from './assets/trouble_brewing_images/ravenkeeper.png'
+import mayor from './assets/trouble_brewing_images/mayor.png'
+import monk from './assets/trouble_brewing_images/monk.png'
+import Investigator from './assets/trouble_brewing_images/Investigator.png'
+import librarian from './assets/trouble_brewing_images/librarian.png'
+import fortune_teller from './assets/trouble_brewing_images/fortune_teller.png'
+import empath from './assets/trouble_brewing_images/empath.png'
+import chef from './assets/trouble_brewing_images/chef.png'
+
+export interface role {
+    src: string,
+    alt: string
+}
+
+// townfolks
+const WASHERWOMAN: role = { src: washerwoman, alt: 'Washerwoman' }
+const UNDERTAKER: role = { src: undertaker, alt: 'Undertaker' }
+const VIRGIN: role = { src: virgin, alt: 'Virgin' }
+const SLAYER: role = { src: slayer, alt: 'Slayer' }
+const SOLDIER: role = { src: soldier, alt: 'Soldier' }
+const RAVENKEEPER: role = { src: ravenkeeper, alt: 'Ravenkeeper' }
+const MAYOR: role = { src: mayor, alt: 'Mayor' }
+const MONK: role = { src: monk, alt: 'Monk' }
+const INVESTIGATOR: role = { src: Investigator, alt: 'Investigator' }
+const LIBRARIAN: role = { src: librarian, alt: 'Librarian' }
+const FORTUNE_TELLER: role = { src: fortune_teller, alt: 'Fortune Teller' }
+const EMPATH: role = { src: empath, alt: 'Empath' }
+const CHEF: role = { src: chef, alt: 'Chef' }
+
+// outsiders
+export const DRUNK: role = { src: drunk, alt: 'Drunk' }
+const RECLUSE: role = { src: recluse, alt: 'Recluse' }
+const SAINT: role = { src: saint, alt: 'Saint' }
+const BUTLER: role = { src: butler, alt: 'Butler' }
+
+// demon
+const IMP: role = { src: imp, alt: 'Imp' }
+
+// minions
+export const BARON: role = { src: baron, alt: 'Baron' }
+const SCARLERT_WOMAN: role = { src: scarlert_woman, alt: 'Scarlert woman' }
+const POISONER: role = { src: poisoner, alt: 'Poisoner' }
+const SPY: role = { src: spy, alt: 'Spy' }
+
+
+export const TOWN_FOLKS = [WASHERWOMAN,
+    UNDERTAKER,
+    VIRGIN,
+    SLAYER,
+    SOLDIER,
+    RAVENKEEPER,
+    MAYOR,
+    MONK,
+    INVESTIGATOR,
+    LIBRARIAN,
+    FORTUNE_TELLER,
+    EMPATH,
+    CHEF]
+export const OUTSIDERS = [DRUNK,
+    RECLUSE,
+    SAINT,
+    BUTLER]
+export const MINIONS = [BARON, SCARLERT_WOMAN, POISONER, SPY]
+export const DEMON = IMP;
